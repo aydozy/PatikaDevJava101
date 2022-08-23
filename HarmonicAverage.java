bad519eef70afe00eefe84fab873eb1734816cd5
Ayda <104395137+aydozy@users.noreply.github.com>
2022-08-23 19:28:35 +0300

    Add files via upload

---
+++ HarmonicAverage.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class HarmonicAverage {
+
+	public static void main(String[] args) {
+
+		int[] numbers = { 1, 2, 3, 4, 5 };
+		double sum = 0.0;
+
+		// Harmonic sum
+		for (int i = 0; i < numbers.length; i++) {
+			sum += (1 / numbers[i]);
+		}
+
+		double averageHarmonic = numbers.length / sum;
+		System.out.println(averageHarmonic);
+
+	}
+
+}
